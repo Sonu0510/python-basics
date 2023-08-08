420096d4a467dfb8e317044313e9f3b3a9d35e41
Yugendra Ray <ray.yugen@gmail.com>
2023-08-09 04:16:03 +0545

    scrap using requests

---
+++ box_plot.py
@@ -0,0 +1,7 @@
+
+"""
+Finding outliers using python
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline
--- web_scrapping.py
+++ web_scrapping.py
@@ -2,60 +2,64 @@
 print("\n")
 
 from bs4 import BeautifulSoup
+import requests
 
-html_code = """
+url = "https://example.com/"
+response = requests.get(url)
 
-<!doctype html>
-<html>
-<head>
-    <title>Example Domain</title>
+# html_code = """
 
-    <meta charset="utf-8" />
-    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <style type="text/css">
-    body {
-        background-color: #f0f0f2;
-        margin: 0;
-        padding: 0;
-        font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+# <!doctype html>
+# <html>
+# <head>
+#     <title>Example Domain</title>
+
+#     <meta charset="utf-8" />
+#     <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+#     <meta name="viewport" content="width=device-width, initial-scale=1" />
+#     <style type="text/css">
+#     body {
+#         background-color: #f0f0f2;
+#         margin: 0;
+#         padding: 0;
+#         font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
         
-    }
-    div {
-        width: 600px;
-        margin: 5em auto;
-        padding: 2em;
-        background-color: #fdfdff;
-        border-radius: 0.5em;
-        box-shadow: 2px 3px 7px 2px rgba(0,0,0,0.02);
-    }
-    a:link, a:visited {
-        color: #38488f;
-        text-decoration: none;
-    }
-    @media (max-width: 700px) {
-        div {
-            margin: 0 auto;
-            width: auto;
-        }
-    }
-    </style>    
-</head>
+#     }
+#     div {
+#         width: 600px;
+#         margin: 5em auto;
+#         padding: 2em;
+#         background-color: #fdfdff;
+#         border-radius: 0.5em;
+#         box-shadow: 2px 3px 7px 2px rgba(0,0,0,0.02);
+#     }
+#     a:link, a:visited {
+#         color: #38488f;
+#         text-decoration: none;
+#     }
+#     @media (max-width: 700px) {
+#         div {
+#             margin: 0 auto;
+#             width: auto;
+#         }
+#     }
+#     </style>    
+# </head>
 
-<body>
-<div>
-    <h1>Example Domain</h1>
-    <p>This domain is for use in illustrative examples in documents. You may use this
-    domain in literature without prior coordination or asking for permission.</p>
-    <p><a href="https://www.iana.org/domains/example">More information...</a></p>
-</div>
-</body>
-</html>
+# <body>
+# <div>
+#     <h1>Example Domain</h1>
+#     <p>This domain is for use in illustrative examples in documents. You may use this
+#     domain in literature without prior coordination or asking for permission.</p>
+#     <p><a href="https://www.iana.org/domains/example">More information...</a></p>
+# </div>
+# </body>
+# </html>
 
-"""
+# """
 
 # Parse the HTML code
-soup = BeautifulSoup(html_code, 'html.parser')
+soup = BeautifulSoup(response.content, 'html.parser')
 
 # Extract header from <h1> element
 h_elements = soup.find_all('h1')
